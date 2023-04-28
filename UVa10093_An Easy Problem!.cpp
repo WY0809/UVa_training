3266aa3c291510674352ee49ff3fa8e51f5b83b1
WY0809 <105807155+WY0809@users.noreply.github.com>
2023-04-28 16:24:25 +0800

    Update UVa10093_An Easy Problem!.cpp

--- UVa10093_An Easy Problem!.cpp
+++ UVa10093_An Easy Problem!.cpp
@@ -5,37 +5,30 @@ int main()
 {
     string s;
     while(getline(cin,s)){
-        int N = 1;
-        int R = 0;
-        bool minus = false;
+        int base = 1;
+        int sum = 0;
         for(int i = 0;i<s.size();i++){
             int temp = 0;
-            if((int)s[i] == 45){
-                minus = true;
-            }else if((int)s[i] >= 97){
-                R+=(int)s[i]-61;
+            if((int)s[i] >= 97){
                 temp = (int)s[i]-61;
             }else if((int)s[i] >= 65){
-                R+=(int)s[i]-55;
                 temp = (int)s[i]-55;
             }else if((int)s[i] >= 48){
-                R+=(int)s[i]-48;
                 temp = (int)s[i]-48;
             }
-            
-            if(temp > N){
-                N = temp;
+            sum += temp;
+            if(temp > base){
+                base = temp;
             }
         }
-        if(minus)
-            R*=-1;
-            
+        
         int ans=0;
-        for(int i = 62 ; i > N ; i--){
-            if(R%(i-1) == 0){
+        for(int i = 62 ; i > base ; i--){
+            if(sum % (i-1) == 0){
                 ans = i;
             }
         }
+        
         if(ans){
             cout<<ans<<endl;
         }else{
