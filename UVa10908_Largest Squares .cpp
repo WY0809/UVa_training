0258dc77156c4392da46f9677a93f26443ea32dd
WY0809 <105807155+WY0809@users.noreply.github.com>
2023-05-05 15:53:00 +0800

    Update UVa10908_Largest Squares .cpp

--- UVa10908_Largest Squares .cpp
+++ UVa10908_Largest Squares .cpp
@@ -4,43 +4,42 @@ using namespace std;
 
 int main()
 {
-	int kase;
-	cin >> kase;
-
-	while (kase--) {
-		int M, N, Q;
-		cin >> M >> N >> Q;
-		cin.get();
-		cout << M << " " << N << " " << Q << endl;
-
-		string str[7];
-		for (int i = 0; i < M; i++) {
-			getline(cin, str[i]);
-			//cout<<str[i]<<endl;
-		}
-
-		while (Q--) {
-			int r, c;
-			cin >> r >> c;
-
-			int length = 1;
-			bool noequal = false;
-			while (1) {
-				for (int i = length * -1; i <= length; i++) {
-					for (int j = length * -1; j <= length; j++) {
-						if (r + i < 0 || r + i >= M || c + j < 0 || c + j >= N || str[r + i][c + j] != str[r][c]) {
-							noequal = true;
-						}
-					}
-				}
-				if (noequal) {
-					break;
-				}
-				length += 2;
-			}
-
-			cout << length << endl;
-		}
-	}
-	return 0;
+    int kase;
+    cin >> kase;
+    
+    while(kase--){
+        int M,N,Q;
+        cin >> M >> N >> Q;
+        cin.get();
+        cout << M <<" "<< N <<" "<< Q << endl;
+        
+        string str[M];
+        for(int i = 0 ; i < M ; i++){
+            getline(cin,str[i]);
+            //cout<<str[i]<<endl;
+        }
+        
+        while(Q--){
+            int r,c;
+            cin>>r>>c;
+            
+            int length = 0,square = 1;
+            bool noequal = false;
+            while(++length){
+                for(int i = length*-1  ; i <= length ; i++){
+                    for(int j = length*-1 ; j <= length ; j++){
+                        if(r+i < 0 || r+i >= M || c+j < 0 || c+j >= N || str[r + i][c + j] != str[r][c]){
+                            noequal = true;
+                        }
+                    }
+                }
+                if(noequal){
+                    break;
+                }
+                square += 2;
+            }
+            cout<<square<<endl;
+        }
+    }
+    return 0;
 }
